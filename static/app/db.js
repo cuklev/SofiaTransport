9e0a2f94505b94376b9909a0e260b8fb78a7ba4d
Kristiyan Tsaklev <cuki@cukii.tk>
2016-05-07 21:19:36 +0300

    Add routes and points handlers

--- static/app/db.js
+++ static/app/db.js
@@ -4,15 +4,15 @@ var db = (function() {
 		var cache = {};
 
 		return function(stopcode) {
-			var data = {
-				stopcode: stopcode
-			};
-
 			var promise = new Promise(function(resolve, reject) {
 				if(cache[stopcode]) {
 					resolve(cache[stopcode]);
 					return;
 				}
+
+				var data = {
+					stopcode: stopcode
+				};
 
 				$.post(url, data, function(stopname) {
 					cache[stopcode] = stopname;
@@ -44,8 +44,68 @@ var db = (function() {
 		};
 	}());
 
+	var getRoutes = (function(line) {
+		var url = 'api/routes';
+		var cache = {}; // is array better?
+
+		return function() {
+			var promise = new Promise(function(resolve, reject) {
+				if(!cache[line.type]) {
+					cache[line.type] = {};
+				}
+				else if(cache[line.type][line.name]) {
+					resolve(cache[line.type][line.name]);
+					return;
+				}
+
+				var data = {
+					linetype: line.type,
+					linename: line.name
+				};
+
+				$.post(url, data, function(routes) {
+					cache[line.type][line.name] = routes;
+					resolve(routes);
+				});
+			});
+
+			return promise;
+		}
+	}());
+
+	var getPoints = (function(line) {
+		var url = 'api/points';
+		var cache = {}; // is array better?
+
+		return function() {
+			var promise = new Promise(function(resolve, reject) {
+				if(!cache[line.type]) {
+					cache[line.type] = {};
+				}
+				else if(cache[line.type][line.name]) {
+					resolve(cache[line.type][line.name]);
+					return;
+				}
+
+				var data = {
+					linetype: line.type,
+					linename: line.name
+				};
+
+				$.post(url, data, function(points) {
+					cache[line.type][line.name] = points;
+					resolve(points);
+				});
+			});
+
+			return promise;
+		}
+	}());
+
 	return {
 		getStopname: getStopname,
-		getLines: getLines
+		getLines: getLines,
+		getRoutes: getRoutes,
+		getPoints: getPoints
 	};
 }());
