837d11f53660b2d4c3024ed8357fcdaa483cbad9
Konstantin Simeonov <kon.simeonov@gmail.com>
2016-07-30 16:23:34 +0300

    Refactored lines controller

--- static/controllers/lines.js
+++ static/controllers/lines.js
@@ -1,49 +1,43 @@
 var linesController = (function() {
 	function get() {
-		var template, lines;
 
-		function update() {
-			if(template === undefined
-			 || lines === undefined) {
-				return;
-			}
+		Promise.all([
+			db.getLines(),
+			templates.get('lines')
+		])
+		.then(function (values) {
+			var lines = values[0],
+				template = values[1];
 
 			$('#linesContainer').html(template(lines));
 
-			$('#linesContainer .tram').on('click', function(e) {
-				var linename = e.target.innerHTML;
-				routesController.get(0, linename);
-			})
-			$('#linesContainer .bus').on('click', function(e) {
-				var linename = e.target.innerHTML;
-				routesController.get(1, linename);
-			})
-			$('#linesContainer .trolley').on('click', function(e) {
-				var linename = e.target.innerHTML;
-				routesController.get(2, linename);
-			})
-		}
+			$('.lines').on('click', 'a', function (ev) {
+				var $target = $(ev.target),
+					transportType = $target
+										.parents('li')
+										.data()['lineType'];
 
-		templates.get('lines').then(function(result) {
-			template = result;
-			update();
+				routesController.get(transportType, $target.text());
+			});
 		});
+	}
+	
+	function filter() {
+		var prefix = $('#enterLinename').val();
+		
+		$('.lines a').each(function (index, element) {
+			var $element = $(element),
+			indexOf = $element.text().toLowerCase().indexOf(prefix);
 
-		db.getLines().then(function(result) {
-			var prefix = $('#enterLinename').val();
-
-			lines = {};
-			for(var i in result) {
-				lines[i] = result[i].filter(function(x) {
-					return (x.indexOf(prefix) === 0); // rewrite me better
-				});
-			}
-
-			update();
+			if(indexOf)
+				$element.addClass('hidden');
+			else
+				$element.removeClass('hidden');
 		});
 	}
 
 	return {
-		get: get
+		get: get,
+		filter: filter
 	};
 }());
