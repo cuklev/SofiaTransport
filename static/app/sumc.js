937c0be63b17a4f92c45ad9b2e088866a8077e4a
Kristiyan Tsaklev <cuki@cukii.tk>
2016-08-20 15:52:33 +0300

    Cache urls in controllers

--- static/app/sumc.js
+++ static/app/sumc.js
@@ -1,27 +1,53 @@
 var sumc = (function() {
-	var baseUrl = 'sumcapi';
+	var baseUrl = '/sumcapi';
 
-	function getTiming(stopcode) {
-		var data = {
-			stopCode: stopcode
+	var getTiming = (function() {
+		var url = baseUrl + '/timing';
+
+		return function(stopCode) {
+			var data = {
+				stopCode: stopCode
+			};
+
+			var promise = new Promise(function(resolve, reject) {
+				$.get(url, data, function(timings) {
+					resolve(timings);
+				});
+			});
+
+			return promise;
 		};
+	}());
 
-		var promise = new Promise(function(resolve, reject) {
-			$.get(baseUrl + '/timing', data, function(timings) {
-				resolve(timings);
+	var getTimetable = (function() {
+		var url = baseUrl + '/timetable';
+
+		return function(stopCode) {
+			var data = {
+				stopCode: stopCode
+			};
+
+			var promise = new Promise(function(resolve, reject) {
+				$.get(url, data, function(timings) {
+					resolve(timings);
+				});
 			});
-		});
 
-		return promise;
-	};
+			return promise;
+		};
+	}());
 
-	function getDatetime() {
-		var promise = new Promise(function(resolve, reject) {
-			$.get(baseUrl + '/datetime', function(datetime) {
-				resolve(datetime);
+	var getDatetime = (function() {
+		var url = baseUrl + '/datetime';
+
+		return function() {
+			var promise = new Promise(function(resolve, reject) {
+				$.get(url, function(datetime) {
+					resolve(datetime);
+				});
 			});
-		});
-	}
+		}
+	}());
 
 	return {
 		getTiming: getTiming,
