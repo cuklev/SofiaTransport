1d6e34641570709fde379c8e55ef0c0e01051f53
Kristiyan Tsaklev <cuki@cukii.tk>
2017-11-18 16:48:33 +0200

    Small rewrite loading code

--- database/load.js
+++ database/load.js
@@ -28,9 +28,9 @@ const getStops = (id) => get(`${baseUrl}/lines/stops/geo?line_id=${id}`)
 								.then(x => JSON.parse(x).features)
 								.catch(() => getStops(id)); // Abuse server
 
-const loadLine = (routes, stops, id) => Promise.all([
-	getRoutes(id).then(x => routes[id] = x),
-	getStops(id).then(x => stops[id] = x.map(x => ({
+const loadLine = (id) => Promise.all([
+	getRoutes(id).then(x => x),
+	getStops(id).then(x => x.map(x => ({
 		id: x.id,
 		code: x.properties.code,
 		name: x.properties.name,
@@ -38,22 +38,37 @@ const loadLine = (routes, stops, id) => Promise.all([
 	})))
 ]);
 
+const loadType = async (id) => {
+	const lines = await getLines(id);
+	await Promise.all(lines.map(async x => {
+		const id = x[0],
+			routesPromise = getRoutes(id),
+			stopsPromise = getStops(id);
+		x[2] = await routesPromise;
+		x[3] = await stopsPromise;
+	}));
+	return lines;
+};
+
 const load = async () => {
 	console.log('Loading DB...');
-	const lines = await Promise.all([1, 2, 3].map(getLines));
+	const data = await Promise.all([1, 2, 3].map(loadType));
 
-	const db = {
+	const db =  {
 		lines: {
-			buses: lines[0],
-			trams: lines[1],
-			trolleys: lines[2],
+			buses: data[0].map(x => x.slice(0, 2)),
+			trams: data[1].map(x => x.slice(0, 2)),
+			trolleys: data[2].map(x => x.slice(0, 2)),
 		},
 		routes: {},
 		stops: {},
 	};
 
-	await Promise.all([].concat(...lines)
-			.map(([id]) => loadLine(db.routes, db.stops, id)));
+	[].concat(...data)
+		.forEach(x => {
+			db.routes[x[0]] = x[2];
+			db.stops[x[0]] = x[3];
+		});
 
 	console.log('Loaded');
 	return db;
