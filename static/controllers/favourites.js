c0b624adcc8d6dce99eeee269a6ffb354b651509
Kristiyan Tsaklev <cuki@cukii.me>
2018-04-14 19:49:12 +0300

    Lambdas in favourites controller

--- static/controllers/favourites.js
+++ static/controllers/favourites.js
@@ -1,9 +1,9 @@
-const favouritesController = (function() {
+const favouritesController = (() => {
 	const STORAGE_KEY = 'fav_stops';
 
 	let favourites = {};
 
-	function load() {
+	const load = () => {
 		const str = localStorage.getItem(STORAGE_KEY) || '';
 
 		try {
@@ -14,67 +14,63 @@ const favouritesController = (function() {
 		}
 
 		get();
-	}
+	};
 
-	function save() {
+	const save = () => {
 		const str = JSON.stringify(favourites);
 		localStorage.setItem(STORAGE_KEY, str);
-	}
+	};
 
-	function rename(target, id) {
+	const rename = (target, id) => {
 		favourites[id] = target.value.trim() || `(${id})`;
 		save();
 		get();
-	}
+	};
 
-	function get() {
-		templates.get('favourites')
-			.then(function(template) {
-				$('#favourites-container').html(template(favourites));
+	const get = async () => {
+		const template = await templates.get('favourites');
+		$('#favourites-container').html(template(favourites));
 
-				// TODO: use a single event
-				$('.edit-favourite').on('click', 'img', function(e) {
-					const $li = $(e.target).parent().parent();
-					const stop_id = $li.data('stopId');
-					const $name = $li.find('a[href]');
-					$li.html(`<input>`);
-					const $input = $li.find('input');
-					$input.val($name.html().trim());
-					$input.focus();
+		// TODO: use a single event
+		$('.edit-favourite').on('click', 'img', (e) => {
+			const $li = $(e.target).parent().parent();
+			const stop_id = $li.data('stopId');
+			const $name = $li.find('a[href]');
+			$li.html(`<input>`);
+			const $input = $li.find('input');
+			$input.val($name.html().trim());
+			$input.focus();
 
-					$input.on('blur', function(e) {
-						rename(e.target, stop_id)
-					});
-					$input.on('keyup', function(e) {
-						if(e.which === 13) {
-							rename(e.target, stop_id)
-						}
-					});
-				});
-				$('.remove-favourite').on('click', 'img', function(e) {
-					const stop_id = $(e.target).parent().parent().data('stopId');
-					remove(stop_id);
-				});
+			$input.on('blur', (e) => rename(e.target, stop_id));
+			$input.on('keyup', (e) => {
+				if(e.which === 13) {
+					rename(e.target, stop_id)
+				}
 			});
-	}
+		});
+		$('.remove-favourite').on('click', 'img', (e) => {
+			const stop_id = $(e.target).parent().parent().data('stopId');
+			remove(stop_id);
+		});
+	};
 
-	function add(stopcode, stopname) {
+	const add = (stopcode, stopname) => {
 		if(favourites.hasOwnProperty(stopcode)) {
 			return;
 		}
 		favourites[stopcode] = `${stopname} (${stopcode})`;
 		save();
 		get();
-	}
+	};
 
-	function remove(stopcode) {
+	const remove = (stopcode) => {
 		delete favourites[stopcode];
 		save();
 		get();
-	}
+	};
 
 	return {
-		load: load,
-		add: add
+		load,
+		add,
 	};
-}());
+})();
