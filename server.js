ee4ead6bcd99a0b324c6b480c410d3bc3765ca5d
Kristiyan Tsaklev <cuki@cukii.me>
2018-04-14 20:23:46 +0300

    jQuery went bye-bye

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const db = require('./database');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/static'));
-app.use('/libs/jquery', express.static(__dirname + '/node_modules/jquery'));
 app.use('/libs/handlebars', express.static(__dirname + '/node_modules/handlebars'));
 app.use('/api', require('./routes/api')(db));
 
