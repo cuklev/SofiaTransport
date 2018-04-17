f1509231c3afee3d2bd1a040dd7ed09bc29e0102
Kristiyan Tsaklev <cuki@cukii.me>
2018-04-17 21:51:19 +0300

    Add simple case insensitive stop searching

--- routes/dbapi.js
+++ routes/dbapi.js
@@ -19,10 +19,19 @@ module.exports = (db, router) => {
 		return db.routes[type][number].map(x => x.codes.map(pairWithName));
 	};
 
+	const searchStops = (searchString) => {
+		const words = searchString.match(/\S+/g)
+			.map(w => w.toUpperCase());
+		return db.stopsList.filter(({n}) => {
+			const nu = n.toUpperCase();
+			return words.every(w => nu.indexOf(w) >= 0);
+		});
+	};
+
 	router
 		.get('/lines', (req, res) => res.send(getAllLines()))
 		.get('/stopname/:code', (req, res) => res.send(getStopname(req.params.code)))
 		.get('/routes/:type/:number', (req, res) => res.send(getRoutes(req.params.type, req.params.number)))
-		.get('/stops', (req, res) => res.send('No stops'))
+		.get('/stops', (req, res) => res.send(searchStops(req.query.s)))
 		.get('/points', (req, res) => res.send('No points'));
 }
