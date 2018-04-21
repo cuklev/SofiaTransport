04de14c8518f58f79cb0380ad0c59d5e0859b924
Kristiyan Tsaklev <cuki@cukii.me>
2018-04-21 14:17:13 +0300

    Show what you are searching

--- static/controllers/search.js
+++ static/controllers/search.js
@@ -2,15 +2,16 @@ const searchInit = (input, container) => {
 	const loadStops = async (str) => {
 		const [template, stops] = await Promise.all([
 			templates.get('search'),
-			db.searchStops(str)
+			(str.length > 0 ? db.searchStops(str) : [])
 		]);
-		container.innerHTML = template(stops);
+		const data = {
+			results: stops,
+			input: str,
+		};
+		container.innerHTML = template(data);
 	};
 
 	input.addEventListener('keyup', (e) => {
-		if(input.value.length === 0) {
-			return;
-		}
 		loadStops(input.value);
 	});
 };
