7638869f1e2ef4dc3ae29b85e1b05f15620306cd
Kristiyan Tsaklev <cuki@cukii.me>
2024-10-06 17:51:05 +0300

    Form the subway routes in timing responses
    the same way we do with routes
    
    * Otherwise, timing responses all appear to be in a single direction

--- routes/sumcapi.js
+++ routes/sumcapi.js
@@ -16,8 +16,18 @@ const timingHandler = async (req, res) => {
 		throw res.statusText;
 	}
 
-	const text = await result.text();
-	res.send(text);
+	const timings = await result.json();
+	for (const timing of Object.values(timings)) {
+		const {route_id, type, name} = timing;
+		if (type === transportTypes.subway) {
+			const routes = await getRoutesCached(type, name);
+			const found = routes.find(r => r.id === route_id);
+			if (found) {
+				timing.route_name = found.name;
+			}
+		}
+	}
+	res.send(JSON.stringify(timings));
 };
 
 const schedulesUrl = 'https://www.sofiatraffic.bg/bg/trip/getSchedule'
@@ -33,41 +43,48 @@ const transportTypes = {
 const routesCache = new Map;
 setInterval(() => routesCache.clear(), 191*60*1000); // clear routes cache every ~3 hours
 
+const getRoutesCached = async (typeNum, name) => {
+	const key = `${typeNum}@${name}`;
+	if (routesCache.has(key)) {
+		return routesCache.get(key);
+	}
+
+	const result = await fetch(schedulesUrl, {
+		method: 'POST',
+		headers: getSessionHeaders(),
+		body: JSON.stringify({
+			ext_id: getExtId(typeNum, name)
+		})
+	});
+	if (!result.ok) {
+		console.error(`Error: ${result.statusText}`);
+		throw res.statusText;
+	}
+
+	const data = await result.json();
+	const routes = data.routes.map(route => {
+		let {id, name} = route;
+		if (typeNum == transportTypes.subway) {
+			const firstStop = route.segments[0].stop.name;
+			const lastStop = route.segments[route.segments.length-1].stop.name;
+			name = `${firstStop} - ${lastStop}`;
+		}
+		return {
+			id,
+			name,
+			stops: route.segments.map(segment => segment.stop.code)
+		};
+	});
+
+	routesCache.set(key, routes);
+	return routes;
+};
+
 const routesHandler = async (req, res) => {
 	const {type, name} = req.params;
 	const typeNum = transportTypes[type] || type;
-
-	const key = `${typeNum}@${name}`;
-	if (!routesCache.has(key)) {
-		const result = await fetch(schedulesUrl, {
-			method: 'POST',
-			headers: getSessionHeaders(),
-			body: JSON.stringify({
-				ext_id: getExtId(typeNum, name)
-			})
-		});
-		if (!result.ok) {
-			console.error(`Error: ${result.statusText}`);
-			throw res.statusText;
-		}
-
-		const data = await result.json();
-		const routes = data.routes.map(route => {
-			let {name} = route;
-			if (typeNum == transportTypes.subway) {
-				const firstStop = route.segments[0].stop.name;
-				const lastStop = route.segments[route.segments.length-1].stop.name;
-				name = `${firstStop} - ${lastStop}`;
-			}
-			return {
-				name,
-				stops: route.segments.map(segment => segment.stop.code)
-			};
-		});
-
-		routesCache.set(key, JSON.stringify(routes));
-	}
-	res.send(routesCache.get(key));
+	const routes = await getRoutesCached(typeNum, name);
+	res.send(JSON.stringify(routes));
 };
 
 module.exports = router => router
